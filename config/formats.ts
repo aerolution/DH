f3ccba77b8070c81e935299639947fabe16d2fb2
Petuuuhhh <90161287+Petuuuhhh@users.noreply.github.com>
2022-03-09 16:59:40 -0500

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -3374,7 +3374,7 @@ export const Formats: FormatList = [
 
 		mod: 'realisticrandom',
 		ruleset: ['Standard', 'Dynamax Clause', 'Data Mod'],
-		banlist: ['Uber', 'Arena Trap', 'Moody', 'Shadow Tag', 'Baton Pass', 'Simple', 'Wonder Guard', 'Shift Gear', 'Shell Smash', 'Quiver Dance', 'Huge Power', 'Pure Power', 'Libero', 'Protean', 'Gorilla Tactics', 'Spore', 'Imposter', 'Moxie', 'Soul-Heart', 'Beast Boost', 'Intrepid Sword', 'Fur Coat', 'Ice Scales', 'Geomancy', 'Disguise'],
+		banlist: ['Uber', 'Arena Trap', 'Moody', 'Shadow Tag', 'Baton Pass', 'Simple', 'Wonder Guard', 'Shift Gear', 'Shell Smash', 'Quiver Dance', 'Huge Power', 'Pure Power', 'Libero', 'Protean', 'Gorilla Tactics', 'Spore', 'Imposter', 'Moxie', 'Soul-Heart', 'Beast Boost', 'Intrepid Sword', 'Fur Coat', 'Ice Scales', 'Geomancy', 'Disguise', 'Speed Boost'],
 	},
 	{
 		name: "[Gen 8] The 3-3-1 Typechart",
